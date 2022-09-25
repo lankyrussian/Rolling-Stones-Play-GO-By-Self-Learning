f4dff429032691ce99b0992555f27d3ab3b77ab4
BobMak <vlmr.mko@gmail.com>
2022-09-24 23:45:23 -0700

    updated README.md; added a base Dockerfile to make ros-noetic image working with bluetooth; removed extra package from setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['goboard', 'kerasclasses'],
+    packages=['goboard'],
     package_dir={'': 'src'},
     requires=[
         'tensorflow',
