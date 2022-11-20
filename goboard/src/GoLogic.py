checking for valid moves using a normal board instead of canonical to fix improper valid move calculation during the game loop; changed 1 to represent black and -1 white, since black move first; checking the game ended with respect to a current player to handle checks with a canonical board; added an artificial rule to end the game if the board is filled with stones of one color in all except one space to speedup initial training; agent wins against random, but gets stuck on self-play after the first update.

goboard/src/GoLogic.py
@@ -46,11 +46,11 @@ class Board():
 
     def get_legal_moves(self, color):
         """Returns all the legal moves for the given color.
-        (1 for white, -1 for black
+        (1 for black, -1 for white
         """
-        # check if already calculated on last move
-        if self.next_legal_moves[0] == Board.get_pos_hash(self.pieces)-color:
-            return self.next_legal_moves[1]
+        # # check if already calculated on last move
+        # if self.next_legal_moves[0] == Board.get_pos_hash(self.pieces)+color:
+        #     return self.next_legal_moves[1]
 
         moves = {self.PASS}  # stores the legal moves.
         # Get all empty locations.
@@ -62,15 +62,15 @@ class Board():
             self.game_ended = True
         return list(moves)
 
-    def get_game_ended(self):
+    def get_game_ended(self, player):
         if not self.game_ended:
             return 0
         # game ended
-        black_score = self.calculate_score(-1)
-        white_score = self.calculate_score(1)
-        if black_score > white_score:
+        player_score = self.calculate_score(player)
+        opponent_score = self.calculate_score(-player)
+        if player_score > opponent_score:
             return 1
-        elif black_score < white_score:
+        elif player_score < opponent_score:
             return -1
         return 0.5
 
@@ -199,5 +199,10 @@ class Board():
         board_hash = Board.get_pos_hash(self.pieces)
         self.board_history_hash[board_hash] = True
         # pre-caluculate the next player's legal moves,
-        # and see if the game is over in case they have non.
+        # and see if the game is over in case they have none.
         self.next_legal_moves = (board_hash-color, self.get_legal_moves(-color))
+        # to make training simpler, we add one more rule:
+        # if the board is filled with stones of one color,
+        # and one eye is left, the game is over.
+        if np.abs(np.sum(self.pieces))==self.pieces.size-1:
+            self.game_ended = True

goboard/src/playgo.py
@@ -13,7 +13,7 @@ def main():
     parser.add_argument('--name',       type=str, default='goboard')
     parser.add_argument('--log',        type=str, default='log.txt')
     parser.add_argument('--board_size', type=int, default=5)
-    parser.add_argument('--num_games',  type=int, default=2)
+    parser.add_argument('--num_games',  type=int, default=10)
     parser.add_argument('--com_type', type=str, default="mqtt")
     parser.add_argument('--player1',    type=str, default='a')
     parser.add_argument('--player2',    type=str, default='a')
@@ -33,11 +33,9 @@ def main():
         from ROSGoGame import ROSGoGame
         g = ROSGoGame(board_size)
     else:
-        raise ValueError("Invalid communication type, should be wither mqtt or ros")
-    # g = GobangGame(board_size)
+        raise ValueError("Invalid communication type, should be either mqtt or ros")
 
     # all players
-
     players = {
         "r": RandomPlayer(g).play,
         "g": GreedyGobangPlayer(g).play,
@@ -49,8 +47,7 @@ def main():
     if player1[0] == 'a':
         # nnet players
         n1 = nn(g)
-        # todo provide a model file
-        n1.load_checkpoint('./pretrained/', 'best.pth.tar')
+        n1.load_checkpoint('./pretrained/', 'checkpoint_3.pth.tar')
         args1 = dotdict({'numMCTSSims': 50, 'cpuct': 1.0})
         mcts1 = MCTS(g, n1, args1)
         player1 = lambda x: np.argmax(mcts1.getActionProb(x, temp=0))
@@ -59,7 +56,7 @@ def main():
 
     if player2[0] == 'a':
         n2 = nn(g)
-        n2.load_checkpoint('/home/vlad/PycharmProjects/Rolling-Stones-Play-GO-By-Self-Learning/goboard/src/pretrained', 'best.pth.tar')
+        n2.load_checkpoint('/home/vlad/PycharmProjects/Rolling-Stones-Play-GO-By-Self-Learning/goboard/src/pretrained', 'checkpoint_3.pth.tar')
         args2 = dotdict({'numMCTSSims': 50, 'cpuct': 1.0})
         mcts2 = MCTS(g, n2, args2)
         n2p = lambda x: np.argmax(mcts2.getActionProb(x, temp=0))

goboard/test/GoGameTests.py
@@ -6,6 +6,10 @@ import unittest
 import src
 # sys.path.append('goboard')
 from src.GoLogic import Board
+
+from GoGame import GoGame
+
+
 # from GoLogic import Board
 
 
@@ -45,7 +49,7 @@ class TestGoLogic(unittest.TestCase):
         self.assertEqual(Board.count_liberties(board, (2, 0)), 7)
         self.assertEqual(Board.count_liberties(board, (2, 2)), 7)
 
-    def test_repetition(self):
+    def test_repetition_black(self):
         board = Board(5)
         board.execute_move((0, 1), 1)
         board.execute_move((1, 0), 1)
@@ -59,6 +63,25 @@ class TestGoLogic(unittest.TestCase):
         valid_moves = board.get_legal_moves(1)
         self.assertNotIn((1, 2), valid_moves)
         self.assertIn((3, 3), valid_moves)
+
+    def test_repetition_white(self):
+        game = GoGame(5)
+        board = game.getInitBoard()
+        board.execute_move((0, 1), -1)
+        board.execute_move((1, 0), -1)
+        board.execute_move((1, 2), -1)
+        board.execute_move((2, 1), -1)
+        board.execute_move((0, 2), 1)
+        board.execute_move((2, 2), 1)
+        board.execute_move((1, 3), 1)
+        board.execute_move((1, 1), 1)  # capture
+        # board.execute_move((1, 2), 1) # would be a ko
+        valid_moves = board.get_legal_moves(-1)
+        self.assertNotIn((1, 2), valid_moves)
+        self.assertIn((3, 3), valid_moves)
+        valid_moves = game.getValidMoves(board, -1)
+        self.assertEqual(valid_moves[6], 0)
+        self.assertEqual(valid_moves[15], 1)
 
     def test_capture(self):
         board = Board(5)
@@ -166,17 +189,5 @@ class TestGoLogic(unittest.TestCase):
         for j in range(4):
             b.execute_move((4, j), 1)
 
-        self.assertTrue(b.get_game_ended())
-
-    def test_no_suicide(self):
-        b = Board(5)
-        b.pieces = np.array([
-            [1, 1, 1, 1, 1],
-            [1, 1, 1, 1, 1],
-            [1, 1, 1, 1, 1],
-            [1, 1, 1, 1, 1],
-            [1, 1, 1, 1, 0],
-        ])
-        valid_moves = b.get_legal_moves(-1)
-        self.assertNotIn((4, 4), valid_moves)
+        self.assertTrue(b.get_game_ended(1))
 